9cfac318c41b2e8c298d2f07c6db821d7ec20b63
kirineko <kirinekoneko@gmail.com>
2016-07-13 02:49:17 +0800

    open debug

--- config/web.php
+++ config/web.php
@@ -60,7 +60,7 @@ $config = [
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        'allowedIPs'=>array('127.0.0.1','192.168.31.201'),
+        'allowedIPs'=>array('127.0.0.1','::1', '192.168.0.*', '192.168.31.201'),
     ];
 //}
 
